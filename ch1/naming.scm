45dbd892e648f848decb7b06b84b9d7f1d7fc759
Gianluca Mauro <mauro.gianluca@hotmail.it>
2021-02-17 08:02:40 +0100

    first commit

---
+++ ch1/naming.scm
@@ -0,0 +1,14 @@
+(define size 2)
+
+size
+;Value: 2
+(* 5 size)
+;Value: 10
+
+(define pi 3.14159)
+(define radius 10)
+(* pi (* radius radius))
+;Value: 314.159
+(define circumference (* 2 pi radius))
+circumference
+;Value: 62.8318
